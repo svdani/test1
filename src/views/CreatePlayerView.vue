64c5c52d105cde201f8d8597999d843708d0cbd1
Pepe Valls <josemaria.valls@students.salle.url.edu>
2024-03-11 18:26:00 +0100

    CreatePlayer begin

---
+++ src/views/CreatePlayerView.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+        <label for="exampleFormControlFile1">Example file input</label>
+        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+    </section>
+</template>
