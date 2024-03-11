e42711061e687a6ac10696f411fdfeedb1bf2f89
Pepe Valls <josemaria.valls@students.salle.url.edu>
2024-03-11 19:04:10 +0100

    player manager delete & code in create player view added

--- src/views/CreatePlayerView.vue
+++ src/views/CreatePlayerView.vue
@@ -1,6 +1,19 @@
 <template>
-    <section>
-        <label for="exampleFormControlFile1">Example file input</label>
-        <input type="file" class="form-control-file" id="exampleFormControlFile1">
-    </section>
+    <header>
+        <h1>Player Creation</h1>
+    </header>
+    <main>
+        <flex>
+            <label for="exampleFormControlFile1">Example file input</label>
+            <input type="file" class="form-control-file" id="imageFile">
+        </flex>
+        <flex>
+            <input type="text" class="form-control" placeholder="Player name">
+        </flex>
+    </main>
+    <footer>
+        <flex>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </flex>
+    </footer>
 </template>
