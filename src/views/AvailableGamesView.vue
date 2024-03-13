8db897696608f6f614c813e66be8ee4773e206ba
JamesHimselff <pv.borras.jaume@gmail.com>
2024-03-13 19:21:05 +0100

    Commit de prova

---
+++ src/views/AvailableGamesView.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <h1>Player Creation</h1>
+    </header>
+    <main>
+        <flex>
+            <label for="exampleFormControlFile1">Example file input</label>
+            <input type="file" class="form-control-file" id="imageFile">
+        </flex>
+        <flex>
+            <input type="text" class="form-control" placeholder="Player name">
+        </flex>
+    </main>
+    <footer>
+        <flex>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </flex>
+    </footer>
+</template>
