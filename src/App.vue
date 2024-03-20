18c5142979642d5da8054da23e510fc3a6f47d67
svdani <dani.hockey.88@gmail.com>
2024-03-20 17:31:19 +0100

    menu con header de prueba

--- src/App.vue
+++ src/App.vue
@@ -1,137 +1,44 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="app">
+    
+
+    <!-- Botón de hamburguesa -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <span class="burger-menu material-symbols-outlined" @click="toggleMenu">menu</span>
 
 
-
-      <!-- start nav -->
-      <nav id="menu">
-      <!-- start menu -->
+    <!-- Menú -->
+    <nav class="menu" :class="{ 'open': isMenuOpen }">
+      <!-- <button class="close-button" @click="closeMenu">Cerrar</button>-->
       <ul>
-        <li><a href="#">Player Management</a>
-          <ul>
-            <li><RouterLink to="/createPlayer">Create Player</RouterLink></li>
-            <li><RouterLink to="/joinView">Join</RouterLink></li>
-            <li><RouterLink to="/playerInfo">Player Info</RouterLink></li>
-            <li><RouterLink to="/deletePlayer">Player Management</RouterLink></li>
-          </ul>
-        </li>
-        <li><a href="#">Enlace 2</a>
-          <ul>
-            <li><a href="#">Enlace 2.1</a></li>
-            <li><a href="#">Enlace 2.2</a></li>
-            <li><a href="#">Enlace 2.3</a></li>
-          </ul>
-        </li>
-        <li><a href="#">Enlace 3</a>
-          <ul>
-            <li><a href="#">Enlace 2.1</a></li>
-            <li><a href="#">Enlace 2.2</a></li>
-            <li><a href="#">Enlace 2.3</a></li>
-          </ul>
-        </li>
-        <li><a href="#">Enlace 4</a>
-          <ul>
-            <li><a href="#">Enlace 2.1</a></li>
-            <li><a href="#">Enlace 2.2</a></li>
-            <li><a href="#">Enlace 2.3</a></li>
-          </ul>
-        </li>
-        <li><a href="#">Enlace 5</a>
-          <ul>
-            <li><a href="#">Enlace 2.1</a></li>
-            <li><a href="#">Enlace 2.2</a></li>
-            <li><a href="#">Enlace 2.3</a></li>
-          </ul>
-        </li>
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/about">About</RouterLink></li>    
       </ul>
-      <!-- end menu -->
-      </nav>
-      <!-- end nav -->
-      
-
-      <!--
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/playerManagement">Player Management</RouterLink>
-        <RouterLink to="/attackManagement">Attack Management</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-      -->
-    </div>
-  </header>
-
-  <RouterView />
+    </nav>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+    closeMenu() {
+      this.isMenuOpen = false;
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+};
+</script>
