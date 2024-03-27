1711f9f05f4ab371dc2b85c6b157ccb9f4774960
Pepe Valls <josem.vallssds@gmail.com>
2024-03-27 17:49:32 +0100

    Create player elements centrats

--- src/views/CreatePlayerView.vue
+++ src/views/CreatePlayerView.vue
@@ -3,17 +3,38 @@
         <h1>Player Creation</h1>
     </header>
     <main>
-        <flex>
-            <label for="exampleFormControlFile1">Example file input</label>
-            <input type="file" class="form-control-file" id="imageFile">
-        </flex>
-        <flex>
-            <input type="text" class="form-control" placeholder="Player name">
-        </flex>
+        <div class="flex-container">
+            <div class="formControl">
+                <input type="file" class="form-control-file" id="imageFile">
+            </div>
+        </div>
+        <div class="flex-container">
+            <div class="formControl">
+                <input type="text" class="form-control" placeholder="Player name">
+            </div>
+        </div>
     </main>
     <footer>
-        <flex>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </flex>
+        <div class="flex-container">
+            <div class="formControl">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
     </footer>
 </template>
+<style>
+.flex-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 25px;
+}
+
+.formControl {
+    margin: 10px;
+}
+
+.form-control, .form-control-file, .btn {
+    margin-top: 10px;
+}
+</style>
