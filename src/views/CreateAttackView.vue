9c20701333ea08fb3f767621eab578c8b3250f95
JamesHimselff <pv.borras.jaume@gmail.com>
2024-03-28 18:10:29 +0100

    CreateAttackView made

---
+++ src/views/CreateAttackView.vue
@@ -0,0 +1,43 @@
+<template>
+    <header>
+      <h1 class="titlelooks">Create Attack </h1>
+      <form>
+        <label for="name">Name</label>
+        <input type="text" class="formlooks"> <br>
+        <label for="x">X</label>
+        <input type="text" class="formlookscoord">
+        <br>
+        <label for="y">Y</label>
+        <input type="text" class="formlookscoord">
+        <br>
+        <button type="submit" class="buttonlooks">Create</button>
+      </form>
+    </header>
+  </template>
+  <style>
+    .titlelooks {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-bottom: 20px;
+    }
+    .formlooks {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    .formlookscoord {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    .labellooks {
+      margin-top: 10px;
+    }
+    .inputlooks {
+      margin-top: 5px;
+    }
+    .buttonlooks {
+      margin-top: 10px;
+    }
+    </style>
