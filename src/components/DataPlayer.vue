361db5c4ab5cfc66d66c74eb2ca7ef6df6cfdf2c
svdani <dani.hockey.88@gmail.com>
2024-04-08 18:33:22 +0200

    Primer componente

---
+++ src/components/DataPlayer.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <button class="btn success btn-right" type="submit" onclick="alert('Hola???')">View Player history</button>
+    <article class="caja_gris" style="width: 95%;" >
+
+        <img class="imagen-circular" src="https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg" alt="Imagen cuadrada">
+        
+        <section style="width: 100%;">
+            <h3>Name</h3>
+            <label for="lvl">Lvl</label>
+
+            <progress id="experience" max="100" value="70">70%</progress>
+        </section>
+    </article>
+</template>
--- src/views/PlayerInfoView.vue
+++ src/views/PlayerInfoView.vue
@@ -1,24 +1,21 @@
-<script>
-    function name() {
-        alert("Hola???");
-    }
-</script>
-
+<!-- src/App.vue -->
 <template>
-
-    <button class="btn success" type="submit" onclick="alert('Hola???')">Create</button>
-    <article class="caja_gris" >
-
-        <img class="imagen-circular" src="https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg" alt="Imagen cuadrada">
-        
-        <section style="width: 100%;">
-            <h3>Name</h3>
-            <label for="lvl">Lvl</label>
-
-            <progress id="experience" max="100" value="70">70%</progress>
-        </section>
-    </article>
-</template>
-
+        <main >
+            <DataPlayer/>
+        </main>
+          
+  </template>
+  
+  <script>
+  import DataPlayer from '../components/DataPlayer.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+        DataPlayer
+    }
+  }
+  </script>
+  
 
 
