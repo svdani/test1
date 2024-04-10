fc3dfd7843905ecbad4e3cf067e2ee3b9c4e962a
JamesHimselff <pv.borras.jaume@gmail.com>
2024-04-10 19:22:37 +0200

    PayerGameHistoryView 1.0

---
+++ src/views/PlayerGameHistoryView.vue
@@ -0,0 +1,63 @@
+<template>
+    
+    <form>
+      
+      <div class="flexaligner2 marginmaker2">
+        <button type="submit" class="buttonlooks success">Statistics</button>
+      </div>
+      <div class="flexaligner2 marginmaker2">
+        <button type="submit" class="buttonlooks success">Game Logs</button>
+      </div>
+      <div class="flexaligner2 marginmaker2">
+        <button type="submit" class="buttonlooks success">Game Replay</button>
+      </div>
+    </form>
+</template>
+<style>
+
+  .marginmaker1{
+    margin-top: 10%;
+  }
+  .marginmaker2{
+    margin-top: 5%;
+  }
+  .flexaligner1{
+    display: flex;
+    flex-direction: center;
+    justify-content: center;
+    text-align: center;
+  }
+  .flexaligner2{
+    display: flex;
+    flex-direction: center;
+    justify-content: center;
+    text-align: center;
+    margin-left: 3%;
+  }
+
+  .titlelooks {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+  .formlooks {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .formlookscoord {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .labellooks {
+    margin-top: 10px;
+  }
+  .inputlooks {
+    margin-top: 5px;
+  }
+  .buttonlooks {
+    margin-top: 10px;
+  }
+  </style>
