9ca8e801cda3bcc1f94f5068e2bd80dc868168b0
svdani <dani.hockey.88@gmail.com>
2024-04-15 18:35:40 +0200

    Bagpack

---
+++ src/components/item-attack-bag.vue
@@ -0,0 +1,14 @@
+<template>
+    <article class="caja_blanca">
+        <section style="display: block;">
+            <h4>Atack name</h4>
+            <h4>X</h4>
+            <h4>Y</h4>
+        </section>
+        
+        <img src="../assets/images/slash.jpg" class="img-attack">
+        <button class="btn-small success " style="">
+            Equip
+        </button>
+    </article>
+</template>
