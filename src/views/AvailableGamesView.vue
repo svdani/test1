fa7a8873353614617cfe70fb8c3f8d8da04ee268
JamesHimselff <pv.borras.jaume@gmail.com>
2024-04-15 18:36:38 +0200

    AvailableGamesView1.3

--- src/views/AvailableGamesView.vue
+++ src/views/AvailableGamesView.vue
@@ -1,5 +1,21 @@
 <template>
-    <div class="caja gris">   
-
+    <div class="caja_gris">
+        <h3>Joinable Games</h3>
+        <article class="caja_blanca caja_blanca_joinable">
+            <p>Available Game 1</p>
+        </article>
+        <article class="caja_blanca caja_blanca_joinable">
+            <p>Available Game 2</p>
+        </article>
+        <article class="caja_blanca caja_blanca_joinable" >
+            <p>Available Game 3</p>
+        </article>
+        <article class="caja_blanca caja_blanca_joinable" >
+            <p>Available Game 4</p>
+        </article>
+        <article class="caja_blanca caja_blanca_joinable" >
+            <p>Available Game 5</p>
+        </article>
+        
     </div>
 </template>
