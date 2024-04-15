f522a935e28e78da26e806f271cacf9c84dcd697
svdani <dani.hockey.88@gmail.com>
2024-04-15 18:51:35 +0200

    Bagpack finiquitado

--- src/components/DataPlayer.vue
+++ src/components/DataPlayer.vue
@@ -7,12 +7,14 @@
             <img class="imagen-circular" src="https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg" alt="Imagen cuadrada">
             
             <section style="width: 100%;">
-                <h3>Name</h3>
-                <div class="caja_dinero"><span>17k $</span></div>
-                
-                <label for="lvl">Lvl</label>
-
-                <progress id="experience" max="100" value="70">70%</progress>
+                <div>
+                    <h3>Name</h3>
+                    <div class="caja_dinero"><span>17k $</span></div>
+                </div>
+                <div>
+                    <label for="lvl">Lvl</label>
+                    <progress id="experience" max="100" value="70">70%</progress>
+                </div>
             </section>
             
         </article>
