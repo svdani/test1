4b7b81b9a1dde5026869819c73f4ead2647997fc
svdani <dani.hockey.88@gmail.com>
2024-04-15 19:13:23 +0200

    DataPlayer finish

--- src/components/DataPlayer.vue
+++ src/components/DataPlayer.vue
@@ -7,11 +7,11 @@
             <img class="imagen-circular" src="https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg" alt="Imagen cuadrada">
             
             <section style="width: 100%;">
-                <div>
+                <div style="display: flex;justify-content: space-between;align-items: center;">
                     <h3>Name</h3>
-                    <div class="caja_dinero"><span>17k $</span></div>
+                    <span style="color:#FFC700;font-size:25px;">17k $</span><!--<div class="caja_dinero"></div>-->
                 </div>
-                <div>
+                <div style="display: flex;justify-content: space-between;align-items: center;">
                     <label for="lvl">Lvl</label>
                     <progress id="experience" max="100" value="70">70%</progress>
                 </div>
