6360842ef310b6f52519a0939ff20e1f4b4ff5cd
svdani <dani.hockey.88@gmail.com>
2024-04-15 18:08:50 +0200

    Botones borrar ataque

--- src/components/item-attack.vue
+++ src/components/item-attack.vue
@@ -1,10 +1,16 @@
 <template>
-    <article class="caja_gris">
+    <article class="caja_blanca">
         <section style="display: block;">
             <h4>Atack name</h4>
             <h4>X</h4>
             <h4>Y</h4>
         </section>
+        <button class="button_overlay" style="padding:4px;">
+            <span class="material-symbols-outlined" style="font-size: xx-large;">
+                close
+            </span>
+        </button>
         <img src="../assets/images/slash.jpg" class="img-attack">
+        
     </article>
 </template>
