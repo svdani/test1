2698fc146701a31278cf76ca41be3e165996f0e0
JamesHimselff <pv.borras.jaume@gmail.com>
2024-04-17 19:58:02 +0200

    buy attack view 1.2

--- src/views/BuyView.vue
+++ src/views/BuyView.vue
@@ -2,40 +2,23 @@
     <header>
         <h3 class="titelPage">Buy Attacks</h3>
     </header>
-    <main class="caja_gris">
+    <main>
         <div class="caja_gris caja_gris_joinable">
       <h3>Joinable Games</h3>
         <article style="display: flex; align-items: center;">
             <section class="caja_blanca caja_blanca_joinable">
-                <p>Available Game 1</p>
+                <p>Kamehameha</p>
+                <p>(0-1)</p>
+                <p>Price</p>
             </section>
             <button class="btn-small success btn-join">Join!</button>
         </article>
+    </div>
+    <div class="caja_gris caja_gris_joinable">
+      <h3>Joinable Games</h3>
         <article style="display: flex; align-items: center;">
             <section class="caja_blanca caja_blanca_joinable">
                 <p>Available Game 2</p>
-            </section>
-            <button class="btn-small success btn-join">Join!</button>
-        </article><article style="display: flex; align-items: center;">
-            <section class="caja_blanca caja_blanca_joinable">
-                <p>Available Game 3</p>
-            </section>
-            <button class="btn-small success btn-join">Join!</button>
-        </article><article style="display: flex; align-items: center;">
-            <section class="caja_blanca caja_blanca_joinable">
-                <p>Available Game 4</p>
-            </section>
-            <button class="btn-small success btn-join">Join!</button>
-        </article>
-        <article style="display: flex; align-items: center;">
-            <section class="caja_blanca caja_blanca_joinable">
-                <p>Available Game 5</p>
-            </section>
-            <button class="btn-small success btn-join">Join!</button>
-        </article>
-        <article style="display: flex; align-items: center;">
-            <section class="caja_blanca caja_blanca_joinable">
-                <p>Available Game 6</p>
             </section>
             <button class="btn-small success btn-join">Join!</button>
         </article>
