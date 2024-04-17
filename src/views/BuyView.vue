b6fd594293cdaa5188808f189492c7e019231686
JamesHimselff <pv.borras.jaume@gmail.com>
2024-04-17 19:28:06 +0200

    BuyView 1.1

--- src/views/BuyView.vue
+++ src/views/BuyView.vue
@@ -1,19 +1,46 @@
 <template>
     <header>
-        <h3 class="titelPage">Player Creation</h3>
+        <h3 class="titelPage">Buy Attacks</h3>
     </header>
     <main class="caja_gris">
-        <flex>
-            <label for="exampleFormControlFile1">Example file input</label>
-            <input type="file" class="form-control-file" id="imageFile">
-        </flex>
-        <flex>
-            <input type="text" class="form-control" placeholder="Player name">
-        </flex>
+        <div class="caja_gris caja_gris_joinable">
+      <h3>Joinable Games</h3>
+        <article style="display: flex; align-items: center;">
+            <section class="caja_blanca caja_blanca_joinable">
+                <p>Available Game 1</p>
+            </section>
+            <button class="btn-small success btn-join">Join!</button>
+        </article>
+        <article style="display: flex; align-items: center;">
+            <section class="caja_blanca caja_blanca_joinable">
+                <p>Available Game 2</p>
+            </section>
+            <button class="btn-small success btn-join">Join!</button>
+        </article><article style="display: flex; align-items: center;">
+            <section class="caja_blanca caja_blanca_joinable">
+                <p>Available Game 3</p>
+            </section>
+            <button class="btn-small success btn-join">Join!</button>
+        </article><article style="display: flex; align-items: center;">
+            <section class="caja_blanca caja_blanca_joinable">
+                <p>Available Game 4</p>
+            </section>
+            <button class="btn-small success btn-join">Join!</button>
+        </article>
+        <article style="display: flex; align-items: center;">
+            <section class="caja_blanca caja_blanca_joinable">
+                <p>Available Game 5</p>
+            </section>
+            <button class="btn-small success btn-join">Join!</button>
+        </article>
+        <article style="display: flex; align-items: center;">
+            <section class="caja_blanca caja_blanca_joinable">
+                <p>Available Game 6</p>
+            </section>
+            <button class="btn-small success btn-join">Join!</button>
+        </article>
+    </div>
     </main>
-    <footer>
-        <flex>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </flex>
-    </footer>
+
+    
 </template>
