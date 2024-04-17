0b8e572659a2ca9230d94f07a97b241452f9c02c
JamesHimselff <pv.borras.jaume@gmail.com>
2024-04-17 20:01:20 +0200

    BuyAttackView 1.3

--- src/views/BuyView.vue
+++ src/views/BuyView.vue
@@ -4,25 +4,32 @@
     </header>
     <main>
         <div class="caja_gris caja_gris_joinable">
-      <h3>Joinable Games</h3>
-        <article style="display: flex; align-items: center;">
-            <section class="caja_blanca caja_blanca_joinable">
-                <p>Kamehameha</p>
-                <p>(0-1)</p>
-                <p>Price</p>
-            </section>
-            <button class="btn-small success btn-join">Join!</button>
-        </article>
-    </div>
-    <div class="caja_gris caja_gris_joinable">
-      <h3>Joinable Games</h3>
-        <article style="display: flex; align-items: center;">
-            <section class="caja_blanca caja_blanca_joinable">
-                <p>Available Game 2</p>
-            </section>
-            <button class="btn-small success btn-join">Join!</button>
-        </article>
-    </div>
+            <h3>Joinable Games</h3>
+            <article style="display: flex; align-items: center;">
+                <section class="caja_blanca caja_blanca_buy_attack">
+                    <p>Kamehameha</p>
+                    <p>(0-1)</p>
+                    <p>2500</p>
+                </section>
+                <button class="btn-small success btn-join">Join!</button>
+            </article>
+            <article style="display: flex; align-items: center;">
+                <section class="caja_blanca caja_blanca_buy_attack">
+                    <p>Genkidama</p>
+                    <p>(1-1)</p>
+                    <p>99999</p>
+                </section>
+                <button class="btn-small success btn-join">Join!</button>
+            </article>
+            <article style="display: flex; align-items: center;">
+                <section class="caja_blanca caja_blanca_buy_attack">
+                    <p>Z Sword</p>
+                    <p>(1-0)</p>
+                    <p>300</p>
+                </section>
+                <button class="btn-small success btn-join">Join!</button>
+            </article>
+        </div>
     </main>
 
     
