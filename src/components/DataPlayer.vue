96e7a0fd199bf5c36cc4cfa41dce55cff097c41a
svdani <dani.hockey.88@gmail.com>
2024-05-06 18:29:24 +0200

    Conexion API Data DataPlayer

--- src/components/DataPlayer.vue
+++ src/components/DataPlayer.vue
@@ -4,15 +4,15 @@
         
         <article class="caja_gris" style="width: 95%;" >
 
-            <img class="imagen-circular" src="https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg" alt="Imagen cuadrada">
-            
+            <img class="imagen-circular" :src="playerInfo ? playerInfo.img : 'https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg'" alt="Profile Image">
+      
             <section style="width: 100%;">
-                <div style="display: flex;justify-content: space-between;align-items: center;">
-                    <h3>Name</h3>
-                    <span style="color:#FFC700;font-size:25px;">17k $</span><!--<div class="caja_dinero"></div>-->
+                <div style="display: flex; justify-content: space-between; align-items: center;">
+                    <h3>{{ playerInfo ? playerInfo.player_ID : 'Name' }}</h3>
+                    <span style="color:#FFC700; font-size:25px;">{{ playerInfo ? `${playerInfo.coins} $` : '' }}</span>
                 </div>
                 <div style="display: flex;justify-content: space-between;align-items: center;">
-                    <label for="lvl">Lvl</label>
+                    <label for="lvl">Lv{{ playerInfo ? playerInfo.level : '0' }}</label>
                     <progress id="experience" max="100" value="70">70%</progress>
                 </div>
             </section>
@@ -24,38 +24,52 @@
 <script>
 import axios from 'axios';
 
-const token = localStorage.getItem("token");
-const user = localStorage.getItem("user");
 
-console.log("token ==" + token);
+
+// URL de la API y token de autenticación
+
 
 export default {
   data() {
     return {
-      playerInfo: {} // Objeto donde almacenarás la información obtenida de la API
+      playerInfo: null // Inicializa playerInfo como null hasta que se carguen los datos
     };
   },
+  mounted() {
+    // URL de la API y token de autenticación
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+    const apiUrl = 'https://balandrau.salle.url.edu/i3/players/'+user;
+    //const token = '1da5e60a-6c2f-4798-bc0e-144a63b7928b';
+    console.log("token =", token)
+    console.log("user =", user)
+    console.log("apiUrl =", apiUrl)
 
-    mounted() {
-        // URL de la API desde donde deseas obtener la información
-        const apiUrl = 'https://balandrau.salle.url.edu/i3/player-info';
+    // Configuración de los encabezados de la solicitud
+    const headers = {
+      'Accept': 'application/json',
+      'Bearer': `${token}`  // Incluye el token de autenticación como Bearer token
+    };
 
-        // Realiza la solicitud GET a la API utilizando axios
-        axios.get(apiUrl)
-        .then(response => {
-            // Verifica si la respuesta es exitosa (estado 200)
-            if (response.status === 200) {
-            // Almacena los datos obtenidos en playerInfo
-            this.playerInfo = response.data;
-            } else {
-            // Si la respuesta no es exitosa, muestra un mensaje de error en la consola
-            console.error('Error al obtener información del jugador:', response.statusText);
-            }
-        })
-        .catch(error => {
-            // Maneja cualquier error que ocurra durante la solicitud
-            console.error('Error al realizar la solicitud:', error);
-        });
-    }
+
+    
+
+
+    // Realiza la solicitud GET a la API utilizando Axios y los encabezados configurados
+    axios.get(apiUrl, { headers })
+      .then(response => {
+        // Verifica si la respuesta es exitosa (estado 200)
+        if (response.status === 200) {
+          this.playerInfo = response.data; // Asigna la respuesta de la API a playerInfo
+          console.log();
+        } else {
+          console.error('Error al obtener datos del jugador:', response.statusText);
+        }
+      })
+      .catch(error => {
+        console.error('Error al realizar la solicitud:', error);
+      });
+  }
 };
+
 </script>
