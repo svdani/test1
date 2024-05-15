9c3cdfc8ee0501843064d9da912ce9ba898dcc3c
JamesHimselff <pv.borras.jaume@gmail.com>
2024-05-15 20:13:02 +0200

    AvailableGamesView Starting components separation

--- src/views/AvailableGamesView.vue
+++ src/views/AvailableGamesView.vue
@@ -9,16 +9,13 @@
                     <button class="btn success btn-right">Search</button>
                 </article>
             </section>
-            <section class="caja_blanca" style="display: flex; flex-direction: column; align-items: flex-end;">
-                <article style="display: flex; align-items: center;">
-                    <label for="horns">Joinable</label>
-                    <input type="checkbox" id="joinable"/>
-                </article>
-                <article style="display: flex; align-items: center;">
-                    <label for="horns">In Progress</label>
-                    <input type="checkbox" id="inProgress"/>
-                </article>
-            </section>
+            
+            
+        <GameFilter />
+
+
+
+
         </div>
         <article style="display: flex; align-items: center;">
             <section class="caja_blanca caja_blanca_joinable" style="justify-content: space-between;">
@@ -70,3 +67,76 @@
 
 
 </style>
+<!---->
+<script>
+import GameFilter from '@/components/GameFilter.vue';
+import axios from 'axios';
+
+export default {
+    components: {
+        GameFilter
+    },
+  data() {
+    return {
+      availableGames: [],
+      joinableGames: [],
+      inProgressGames: [],
+    };
+  },
+  mounted() {
+    this.fetchCurrentArenas();
+  },
+  methods: {
+    fetchCurrentArenas() {
+      const apiUrl = `https://balandrau.salle.url.edu/i3/players/arenas/current`;
+      const token = localStorage.getItem("token");
+
+      const headers = {
+        'Accept': 'application/json',
+        'Bearer': token
+      };
+
+      axios.get(apiUrl, { headers })
+        .then(response => {
+          if (response.status === 200) {
+            // Assuming response.data contains the current arenas data
+            console.log('Current Arenas Data:', response.data);
+
+            // Filter available games
+            this.availableGames = response.data.filter(game => !game.finished);
+
+            if (this.joinableChecked) {
+              this.joinableGames = this.availableGames.filter(game => !game.start);
+            } else {
+              this.joinableGames = [];
+            }
+
+            // Apply in-progress filter if checkbox is checked
+            if (this.inProgressChecked) {
+              this.inProgressGames = this.availableGames.filter(game => game.start);
+            } else {
+              this.inProgressGames = [];
+            }
+
+            // Filter joinable games
+            this.joinableGames = this.availableGames.filter(game => !game.start);
+
+            // Filter in-progress games
+            this.inProgressGames = this.availableGames.filter(game => game.start);
+
+            console.log('Available Games:', this.availableGames);
+            console.log('Joinable Games:', this.joinableGames);
+            console.log('In-progress Games:', this.inProgressGames);
+
+          } else {
+            console.error('Error fetching current arenas:', response.statusText);
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching current arenas:', error);
+        });
+    }
+  }
+}
+</script>
+
