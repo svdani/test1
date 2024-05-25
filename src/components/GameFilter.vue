cba36f6738523486b9939a34f1836250b5411fd0
JamesHimselff <pv.borras.jaume@gmail.com>
2024-05-25 14:37:55 +0200

    Checkbox only allows one filter at a time now

--- src/components/GameFilter.vue
+++ src/components/GameFilter.vue
@@ -2,11 +2,11 @@
     <section class="caja_blanca" style="display: flex; flex-direction: column; align-items: flex-end;">
       <article style="display: flex; align-items: center;">
         <label for="joinable">Joinable</label>
-        <input type="checkbox" id="joinable" v-model="localFilters.joinable" @change="emitFilters" />
+        <input type="checkbox" id="joinable" v-model="localFilters.joinable" @change="updateFilters('joinable')" />
       </article>
       <article style="display: flex; align-items: center;">
         <label for="inProgress">In Progress</label>
-        <input type="checkbox" id="inProgress" v-model="localFilters.inProgress" @change="emitFilters" />
+        <input type="checkbox" id="inProgress" v-model="localFilters.inProgress" @change="updateFilters('inProgress')" />
       </article>
     </section>
   </template>
@@ -22,6 +22,14 @@
       };
     },
     methods: {
+      updateFilters(changedFilter) {
+        if (changedFilter === 'joinable') {
+          this.localFilters.inProgress = false;
+        } else if (changedFilter === 'inProgress') {
+          this.localFilters.joinable = false;
+        }
+        this.emitFilters();
+      },
       emitFilters() {
         this.$emit('update-filters', this.localFilters);
       }
