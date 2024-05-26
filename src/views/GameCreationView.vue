b3fbf4b506426d7d750f851de4973ccdc7c33ace
Pepe Valls <josem.vallssds@gmail.com>
2024-05-26 21:52:57 +0200

    bug fixed

--- src/views/GameCreationView.vue
+++ src/views/GameCreationView.vue
@@ -6,46 +6,45 @@
     </section>
   </main>
 </template>
-
-<script>
-import Board from '../components/Board.vue';
-import GameForm from '../components/GameForm.vue';
-
-export default {
-  components: {
-    Board,
-    GameForm
-  },
-  data() {
-    return {
-      playerHP: 0,
-      boardSize: 0,
-    };
-  },
-  methods: {
-    updateBoardSize(data) {
+  
+  <script>
+  import Board from '../components/Board.vue';
+  import GameForm from '../components/GameForm.vue';
+  
+  export default {
+    components: {
+      Board,
+      GameForm
+    },
+    data() {
+      return {
+        playerHP: 0,
+        boardSize: 0,
+      };
+    },
+    methods: {
+      updateBoardSize(data) {
       this.boardSize = parseInt(data.boardSize);
+      },
     }
-  }
-};
-</script>
-
+  };
+  </script>
 <style>
-body {
-  font-family: Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-}
-.container {
-  max-width: 900px;
-  margin: 20px auto;
-  padding: 20px;
-  border: none;
-  border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-between;
-}
+    body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+    .container {
+        max-width: 900px;
+        margin: 20px auto;
+        padding: 20px;
+        border: none;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
 </style>
